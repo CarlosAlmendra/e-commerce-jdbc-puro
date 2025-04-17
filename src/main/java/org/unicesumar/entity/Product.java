74f5962dba187ad6c09c0b861a8c0c355c939451
Carlos <almendracarloshenrique@gmail.com>
2025-04-17 00:39:20 -0300

    added entity Product

---
+++ src/main/java/org/unicesumar/entity/Product.java
@@ -0,0 +1,33 @@
+package org.unicesumar.entity;
+
+import java.util.UUID;
+
+public class Product extends Entity {
+    private final String name;
+    private final double price;
+
+    public Product(UUID uuid, String name, double price) {
+        super(uuid);
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public String toString() {
+        return "ID: " + getUuid().toString()
+                + "\nName: " + name
+                + "\nPrice: " + price;
+    }
+}
