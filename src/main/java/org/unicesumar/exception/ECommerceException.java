fbf5e1e957ba94dd8c64a395c611837bca9f7c76
Carlos <almendracarloshenrique@gmail.com>
2025-04-17 00:33:44 -0300

    added mine exception

---
+++ src/main/java/org/unicesumar/exception/ECommerceException.java
@@ -0,0 +1,7 @@
+package org.unicesumar.exception;
+
+public class ECommerceException extends RuntimeException {
+    public ECommerceException(String message, String cause) {
+        super(message);
+    }
+}
