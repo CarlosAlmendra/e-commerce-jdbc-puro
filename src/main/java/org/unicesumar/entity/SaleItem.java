2ad22825417777e9971490bb12db16bd69f2893d
Carlos <almendracarloshenrique@gmail.com>
2025-04-18 23:38:09 -0300

    refactor saleItem class

--- src/main/java/org/unicesumar/entity/SaleItem.java
+++ src/main/java/org/unicesumar/entity/SaleItem.java
@@ -1,29 +1,57 @@
 package org.unicesumar.entity;
 
+import java.util.List;
+
 public class SaleItem extends Entity {
-    private final Product product;
-    private final int quantity;
 
-    public SaleItem(Product product, int quantity) {
+    private User user;
+    private List<Product> products;
+    private Double totalValue;
+
+    public SaleItem(List<Product> products) {
+        this.products = products;
+    }
+
+    public SaleItem(User user, List<Product> products, double totalValue) {
         super();
-        this.product = product;
-        this.quantity = quantity;
+        this.user = user;
+        this.products = products;
+        this.totalValue = totalValue;
     }
 
-    public Product getProduct() {
-        return product;
+    public User getUser() {
+        return user;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public void setUser(User user) {
+        this.user = user;
     }
 
-    public double getTotalPrice() {
-        return product.getPrice() * quantity;
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public Double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(Double totalValue) {
+        this.totalValue = totalValue;
     }
 
     @Override
     public String toString() {
-        return quantity + " x " + product.getName() + " - $" + getTotalPrice();
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n===Sales date ===\n");
+        sb.append("User name: ").append(user.getName()).append("\n");
+        sb.append("Products:\n");
+        products.forEach(p -> sb.append("- ").append(p.getName()).append("\n"));
+        sb.append("Total value: R$ ").append(totalValue).append("\n");
+        sb.append("Payment type: ").append("PIX").append("\n");
+        return sb.toString();
     }
 }
