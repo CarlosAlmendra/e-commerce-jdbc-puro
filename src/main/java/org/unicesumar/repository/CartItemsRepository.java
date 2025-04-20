b372fef2e53893b9cd90c06b370438f239956fa7
Carlos <almendracarloshenrique@gmail.com>
2025-04-20 11:17:40 -0300

    added find all products by cart method in Cart Items repository class

--- src/main/java/org/unicesumar/repository/CartItemsRepository.java
+++ src/main/java/org/unicesumar/repository/CartItemsRepository.java
@@ -1,10 +1,15 @@
 package org.unicesumar.repository;
 
+import org.unicesumar.entity.Cart;
 import org.unicesumar.entity.CartItems;
+import org.unicesumar.entity.Product;
+import org.unicesumar.entity.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -32,6 +37,52 @@ public class CartItemsRepository implements EntityRepository<CartItems>{
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public List<CartItems> findAllProductsByCart(UUID idCart) {
+        UserRepository userRepository = new UserRepository(connection);
+        CartRepository cartRepository = new CartRepository(connection);
+        ProductRepository productRepository = new ProductRepository(connection);
+
+        String query = "SELECT * FROM cart_items WHERE cart_id = ?";
+        List<CartItems> cartItems = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setString(1, idCart.toString());
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()) {
+                UUID userId = UUID.fromString(resultSet.getString("user_id"));
+                UUID productId = UUID.fromString(resultSet.getString("product_id"));
+                UUID cartId = UUID.fromString(resultSet.getString("cart_id"));
+
+                User user = userRepository.findById(userId)
+                        .orElseThrow(() -> new RuntimeException("User not found: " + userId));
+
+                Product product = productRepository.findById(productId)
+                        .orElseThrow(() -> new RuntimeException("Product not found: " + productId));
+
+                Cart cart = cartRepository.findById(cartId)
+                        .orElseThrow(() -> new RuntimeException("Cart not found: " + cartId));
+
+
+                CartItems cartItem = new CartItems(
+                        UUID.fromString(resultSet.getString("uuid")),
+                        user,
+                        product,
+                        cart,
+                        resultSet.getInt("quantity"),
+                        resultSet.getDouble("unit_price")
+                );
+                cartItems.add(cartItem);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return cartItems;
     }
 
     @Override
