c69022ade9c0e925c1fd637ace06a38b68fc3313
Carlos <almendracarloshenrique@gmail.com>
2025-04-19 23:42:17 -0300

    refactored Sale class

---
+++ src/main/java/org/unicesumar/entity/Sale.java
@@ -0,0 +1,59 @@
+package org.unicesumar.entity;
+
+import org.unicesumar.enums.PaymentEnum;
+
+import java.util.UUID;
+
+public class Sale extends Entity {
+    private User user;
+    private Cart cart;
+    private double totalValue;
+    private PaymentEnum paymentEnum;
+
+    public Sale(User user, Cart cart, double totalValue, PaymentEnum paymentEnum) {
+        this.user = user;
+        this.cart = cart;
+        this.totalValue = totalValue;
+        this.paymentEnum = paymentEnum;
+    }
+
+    public Sale(UUID uuid, User user, Cart cart, double totalValue, PaymentEnum paymentEnum) {
+        super(uuid);
+        this.user = user;
+        this.cart = cart;
+        this.totalValue = totalValue;
+        this.paymentEnum = paymentEnum;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+    public PaymentEnum getPaymentEnum() {
+        return paymentEnum;
+    }
+
+    public void setPaymentEnum(PaymentEnum paymentEnum) {
+        this.paymentEnum = paymentEnum;
+    }
+}
--- src/main/java/org/unicesumar/exception/ECommerceException.java
+++
@@ -1,7 +0,0 @@
-package org.unicesumar.exception;
-
-public class ECommerceException extends RuntimeException {
-    public ECommerceException(String message, String cause) {
-        super(message);
-    }
-}
