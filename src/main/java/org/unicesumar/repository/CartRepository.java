4e49e951b234c56f1d7dd8a42430b26306ffc6cc
Carlos <almendracarloshenrique@gmail.com>
2025-04-20 11:17:56 -0300

    add find by id method in Cart repository class

--- src/main/java/org/unicesumar/repository/CartRepository.java
+++ src/main/java/org/unicesumar/repository/CartRepository.java
@@ -1,9 +1,11 @@
 package org.unicesumar.repository;
 
 import org.unicesumar.entity.Cart;
+import org.unicesumar.entity.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
@@ -32,6 +34,28 @@ public class CartRepository implements EntityRepository<Cart> {
 
     @Override
     public Optional<Cart> findById(UUID id) {
+        UserRepository userRepository = new UserRepository(connection);
+
+        String query = "SELECT * FROM cart WHERE uuid = ?";
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setString(1, id.toString());
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                UUID userId = UUID.fromString(resultSet.getString("user_id"));
+
+                User user = userRepository.findById(userId)
+                        .orElseThrow(() -> new RuntimeException("User not found: " + userId));
+
+                return Optional.of(new Cart(
+                        UUID.fromString(resultSet.getString("uuid")),
+                        user
+                ));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         return Optional.empty();
     }
 
